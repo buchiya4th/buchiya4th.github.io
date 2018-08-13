f153390b014103e783e048e792a92cbd0e757799
buchiya4th <buchiya4th@gmail.com>
2018-08-14 07:21:24 +0900

    add: プリプロセッサの追加とindexの単一ファイルコンポーネント化

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,12 @@
-<template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        buchiya4th-portfolio
-      </h1>
-      <h2 class="subtitle">
-        プロジェクト！
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
-</template>
+<template
+  src="./index.pug"
+  lang="pug"
+  />
 
-<script>
-import AppLogo from '~/components/AppLogo.vue'
+<script src="./index.js" />
 
-export default {
-  components: {
-    AppLogo
-  }
-}
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style
+  src="./index.scss"
+  lang="scss"
+  />
 
