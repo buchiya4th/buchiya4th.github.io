e413d6fa52391d460e5e2ff211cdeef74256e258
buchiya4th <buchiya4th@gmail.com>
2018-09-03 08:04:07 +0900

    add: footerの追加

---
+++ components/TheFooter/TheFooter.vue
@@ -0,0 +1,12 @@
+<template
+  src="./TheFooter.pug"
+  lang="pug"
+  />
+
+<script src="./TheFooter.js" />
+
+<style
+  src="./TheFooter.scss"
+  lang="scss"
+  scoped
+  />
--- layouts/default.js
+++ layouts/default.js
@@ -1,10 +1,12 @@
 import TheHeader from '@/components/TheHeader/TheHeader.vue'
+import TheFooter from '@/components/TheFooter/TheFooter.vue'
 
 export default {
   data() {
     return {}
   },
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 }
