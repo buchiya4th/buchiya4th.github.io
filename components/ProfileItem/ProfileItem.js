86579b0dcb2a9e44b70291081e052aa5ad071e1a
buchiya4th <buchiya4th@gmail.com>
2021-01-21 07:14:24 +0900

    clean: components配下のファイルを整理

--- components/ProfileItem/ProfileItem.js
+++
@@ -1,9 +0,0 @@
-export default {
-  data() {
-    return {
-      userName: this.$store.state.userName,
-      profileImage: this.$store.state.profileItem.profileImage,
-      profileSites: this.$store.state.profileItem.profileSites
-    }
-  }
-}
--- components/TheHeroHeaderTop/TheHeroHeaderTop.js
+++
@@ -1,7 +0,0 @@
-import ProfileItem from '@/components/ProfileItem/ProfileItem.vue'
-
-export default {
-  components: {
-    ProfileItem
-  }
-}
