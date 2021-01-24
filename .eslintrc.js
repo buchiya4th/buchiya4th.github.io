897212e4b83de3991c75905b47ee39643b6cea1d
buchiya4th <buchiya4th@gmail.com>
2021-01-25 06:44:34 +0900

    change: lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  ignorePatterns: ['/docs'],
   env: {
     browser: true,
     node: true,
@@ -9,23 +10,12 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/recommended',
-    'plugin:prettier/recommended',
   ],
-  // required to lint *.vue files
   plugins: ['vue'],
-  // add your custom rules here
   rules: {
     semi: [2, 'never'],
+    quotes: ['error', 'single'],
     'no-console': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        semi: false,
-      },
-    ],
   },
 }
