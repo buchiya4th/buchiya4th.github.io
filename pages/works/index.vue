e898ab73fb641f89b2b03a7d6b24ba03f82ae15b
buchiya4th <buchiya4th@gmail.com>
2021-02-08 19:19:04 +0900

    add: works page

---
+++ pages/works/index.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+main.main
+  h1.ttl-a {{ title }}
+  .container
+    WorkList
+</template>
+
+<script>
+import { defineComponent, computed } from '@nuxtjs/composition-api'
+import WorkList from '@/components/services/works/list'
+
+export default defineComponent({
+  components: {
+    WorkList
+  },
+  setup() {
+    const title = computed(() => 'Works')
+    const metaDescription = computed(() => '作品ページです。')
+    const bodyClass = computed(() => 'page-works')
+
+    return {
+      title,
+      metaDescription,
+      bodyClass,
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.metaDescription,
+        },
+      ],
+      bodyAttrs: {
+        class: this.bodyClass,
+      },
+    }
+  },
+})
+</script>
