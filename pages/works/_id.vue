3ceaef2282f947fd1e60b7fb79003fb22cb9acbf
buchiya4th <buchiya4th@gmail.com>
2022-01-12 10:28:11 +0900

    chore: change directory structure for works

--- pages/works/_id.vue
+++ pages/works/_id.vue
@@ -11,73 +11,24 @@ main.main
         :href="item.siteUrl"
         target="_blank"
       ) {{ item.siteUrl }}
-    .imageSlider
-      swiper(
-        ref="mySwiper"
-        :options="swiperOptions"
-      )
-        swiper-slide(
-          v-for="(item, index) in item.images"
-          :key="index"
-        )
-          img.slideImage(
-            :src="item.src"
-            width="800"
-            height="450"
-            alt=""
-            loading="lazy"
-          )
-      .swiper-pagination(slot="pagination")
-      .swiper-button-prev(slot="button-prev")
-      .swiper-button-next(slot="button-next")
-    table.workTable
-      tbody
-        tr
-          th サイト名
-          td {{ item.title }}
-        tr
-          th URL
-          td
-            a(
-              :href="item.siteUrl"
-              target="_blank"
-            ) {{ item.siteUrl }}
-        tr
-          th 使用技術
-          td
-            ul.inline-list
-              li(
-                v-for="(item, index) in item.useTechnology"
-                :key="index"
-              ) {{ item }}
-        tr
-          th 使用ツール
-          td
-            ul.inline-list
-              li(
-                v-for="(item, index) in item.useTools"
-                :key="index"
-              ) {{ item }}
-        tr
-          th 概要
-          td {{ item.outline }}
+    WorksImageSlider(:images="item.images")
+    WorksTable(:item="item")
+
   FooterContentContact
 </template>
 
 <script>
-import { computed, defineComponent, ref } from '@nuxtjs/composition-api'
-import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
+import { computed, defineComponent } from '@nuxtjs/composition-api'
+import WorksImageSlider from '@/components/services/works/WorksImageSlider.vue'
+import WorksTable from '@/components/services/works/WorksTable.vue'
 import FooterContentContact from '@/components/organisms/FooterContentContact.vue'
 
 export default defineComponent({
   name: 'WorksDetail',
   components: {
-    Swiper,
-    SwiperSlide,
-    FooterContentContact
-  },
-  directives: {
-    swiper: directive
+    WorksImageSlider,
+    WorksTable,
+    FooterContentContact,
   },
   setup() {
     const title = computed(() => 'Works')
@@ -111,29 +62,12 @@ export default defineComponent({
         },
       ]
     })
-    const mySwiper = ref(null)
-    const swiperOptions = ref({
-      speed: 600,
-      spaceBetween: 30,
-      centeredSlides: true,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-    }).value
 
     return {
       title,
       metaDescription,
       bodyClass,
       content,
-      mySwiper,
-      swiperOptions,
     }
   }
 })
@@ -157,99 +91,5 @@ export default defineComponent({
 .siteUrl {
   margin-top: 0;
   text-align: center;
-}
-
-.imageSlider {
-  --swiper-navigation-size: 24px;
-  --swiper-navigation-color: #666;
-  --swiper-pagination-color: #666;
-
-  position: relative;
-  margin: size(4, px) 0 size(5.5, px);
-
-  @media (min-width: "500px") {
-    --swiper-navigation-size: 40px;
-  }
-}
-
-.swiper-container {
-  width: calc(100% - 80px);
-
-  @media (min-width: "500px") {
-    width: calc(100% - 160px);
-    max-width: 800px;
-  }
-}
-
-.swiper-pagination {
-  right: 0;
-  left: 0;
-  margin: auto;
-
-  /deep/ .swiper-pagination-bullet {
-    width: 10px;
-    height: 10px;
-    margin-right: 4px;
-    margin-left: 4px;
-  }
-}
-
-.swiper-button-prev:focus,
-.swiper-button-next:focus,
-.swiper-pagination /deep/ .swiper-pagination-bullet:focus {
-  outline: none;
-}
-
-.slideImage {
-  border: 1px solid #ccc;
-}
-
-.workTable {
-  max-width: 100%;
-  word-break: break-word;
-
-  @media (min-width: "700px") {
-    width: calc(100% - 160px);
-    max-width: 800px;
-    margin: 0 auto;
-  }
-
-  th,
-  td {
-    @media (max-width: "699px") {
-      display: block;
-    }
-  }
-
-  th {
-    min-width: 6em;
-    padding: size(1, px) size(1, px) size(1, px) 0;
-
-    @media (max-width: "699px") {
-      padding: size(1, px) size(1, px) 0;
-    }
-  }
-
-  td {
-    padding: size(1, px) 0 size(1, px) size(1, px);
-
-    @media (max-width: "699px") {
-      padding: 0 size(1, px) size(1, px);
-    }
-  }
-}
-
-.inline-list {
-  margin: 0;
-  padding: 0;
-
-  li {
-    display: inline-block;
-
-    &:not(:last-child)::after {
-      margin-right: 0.5em;
-      content: ",";
-    }
-  }
 }
 </style>
