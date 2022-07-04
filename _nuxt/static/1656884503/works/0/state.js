f898530e6c17a7cf7c0152869643fbf205ff0aba
buchiya4th <buchiya4th@gmail.com>
2022-07-05 06:26:40 +0900

    delete: all

--- _nuxt/static/1656884503/works/0/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbuchiya4th.github.io\u002F_nuxt\u002Fstatic\u002F1656884503",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002F0",config:{_app:{basePath:"\u002Fbuchiya4th.github.io\u002F",assetsPath:"\u002Fbuchiya4th.github.io\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
